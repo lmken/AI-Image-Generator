b8ee91a67fc5dadc3f719f83b3d63b59c0cb2509
Luke Kennedy <lukemkenne@gmail.com>
2023-01-29 15:25:21 -0500

    cleaned some things up visually

--- index.py
+++ index.py
@@ -26,15 +26,15 @@ def createImage(userInput):
     img.show()
 
 
-def editImage():
-    response = openai.Image.create_edit(
-        image=open("sunlit_lounge.png", "rb"),
-        mask=open("mask.png", "rb"),
-        prompt="A sunlit indoor lounge area with a pool containing a flamingo",
-        n=1,
-        size="1024x1024"
-    )
-    image_url = response['data'][0]['url']
+# def editImage():
+#     response = openai.Image.create_edit(
+#         image=open("sunlit_lounge.png", "rb"),
+#         mask=open("mask.png", "rb"),
+#         prompt="A sunlit indoor lounge area with a pool containing a flamingo",
+#         n=1,
+#         size="1024x1024"
+#     )
+#     image_url = response['data'][0]['url']
 
 
 begin()
